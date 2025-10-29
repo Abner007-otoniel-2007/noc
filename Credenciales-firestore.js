18d34f4b4e38c5dae2ada48777098b5637c374cf
Abner007-otoniel-2007 <abner.gonzalez@oportunidades.org.sv>
2025-10-28 23:00:45 -0600

    Add files via upload

---
+++ Credenciales-firestore.js
@@ -0,0 +1,15 @@
+
+firebase.initializeApp ({
+      apiKey: "AIzaSyBRYLVtSDethYfsHIC9RZFd2mQzl3iMA4Y",
+  authDomain: "database-5ed49.firebaseapp.com",
+  projectId: "database-5ed49",
+  storageBucket: "database-5ed49.firebasestorage.app",
+  messagingSenderId: "664248216667",
+  appId: "1:664248216667:web:8e466d212138d2b18f237e"
+});
+
+
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
